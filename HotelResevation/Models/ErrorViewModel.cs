095890c9680422d1dc1e59981950d519afddd1c0
arman ruzbahani <armanruzbahani100@gmail.com>
2025-05-10 11:56:44 -0900

    Add project files.

---
+++ HotelResevation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HotelResevation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
